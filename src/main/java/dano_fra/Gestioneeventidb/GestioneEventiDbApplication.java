7e57a974111f2ca34226dde70a4ec600639f86e3
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-04-19 10:48:27 +0200

    Initial commit

---
+++ src/main/java/dano_fra/Gestioneeventidb/GestioneEventiDbApplication.java
@@ -0,0 +1,13 @@
+package dano_fra.Gestioneeventidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dano_fra/Gestioneeventidb/GestioneEventiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dano_fra.Gestioneeventidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
