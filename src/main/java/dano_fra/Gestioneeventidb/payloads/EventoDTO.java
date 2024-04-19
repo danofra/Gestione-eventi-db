d4840147ca677d3b7c991b64a6e0b8e425f5eedf
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-04-19 11:40:50 +0200

    mod eventODTO

--- src/main/java/dano_fra/Gestioneeventidb/payloads/EventoDTO.java
+++ src/main/java/dano_fra/Gestioneeventidb/payloads/EventoDTO.java
@@ -3,6 +3,8 @@ package dano_fra.Gestioneeventidb.payloads;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
+
 public record EventoDTO(
         @NotNull(message = "Nome non può essere nullo")
         @NotEmpty(message = "Nome non può essere vuoto")
@@ -12,10 +14,12 @@ public record EventoDTO(
         String descrizione,
         @NotNull(message = "Data non può essere nullo")
         @NotEmpty(message = "Data non può essere vuota")
-        String data,
+        LocalDate data,
         @NotNull(message = "Luogo non può essere nullo")
         @NotEmpty(message = "Luogo non può essere vuoto")
         String luogo,
+        @NotNull(message = "Max partecipanti non può essere nullo")
+        @NotEmpty(message = "Max partecipanti non può essere vuoto")
         int max_partecipanti
 ) {
 }
